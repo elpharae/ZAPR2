c6c71e026b67d88591d4b05dd7f19b9db33c09b7
elpharaexd <elpharaexd@outlook.com>
2021-09-27 16:15:41 +0200

    Hotovy program

--- src/pkg/Main.java
+++ src/pkg/Main.java
@@ -38,9 +38,7 @@ public class Main {
         return new double[]{2 * Math.PI * radius, Math.PI * Math.pow(radius, 2)};
     }
 
-    private Main() {
-        this.sc = new Scanner(System.in);
-
+    private void homework() {
         System.out.print("Zadej galony: ");
         System.out.println(this.galToLi(sc.nextFloat()));
 
@@ -50,6 +48,58 @@ public class Main {
         System.out.print("Zadej poloměr kruhu: ");
         double[] result = this.perimAndArea(sc.nextDouble());
         System.out.println("Obvod: " + result[0] + ", Obsah: " + result[1]);
+    }
+
+    // 1. zadání - načti přes scanner jméno a přijmení a vypiš je do konzole
+    private void exc1() {
+        String[] firstLast = new String[2];
+
+        System.out.println("Zadej jméno: ");
+        firstLast[0] = sc.next();
+
+        System.out.println("Zadej přijmení: ");
+        firstLast[1] = sc.next();
+
+        System.out.println(firstLast[0] + " " + firstLast[1]);
+    }
+
+    // 2. zadání - načti číslo přes scanner a vypiš, zda je kladné, záporné či nula
+    private void exc2() {
+        int num = sc.nextInt();
+
+        if (num < 0) System.out.println("Záporné");
+        else if (num > 0) System.out.println("Kladné");
+        else System.out.println("Nula");
+    }
+
+    // 3. zadání - načti 3 čísla přes scanner a vypiš největší
+    private void exc3() {
+        int a, b, c;
+        System.out.println("Zadej číslo A");
+        a = sc.nextInt();
+        System.out.println("Zadej číslo B");
+        b = sc.nextInt();
+        System.out.println("Zadej číslo C");
+        c = sc.nextInt();
+
+        System.out.println("Největší je: ");
+
+        if (a > b) {
+            if (a > c) System.out.print(a);
+            else System.out.print(c);
+        } else if (b > a) {
+            if (b > c) System.out.print(b);
+            else System.out.print(c);
+        } else System.out.print(c);
+    }
+
+    private Main() {
+        this.sc = new Scanner(System.in);
+
+        //exc1();
+        //exc2();
+        exc3();
+
     }
 
     public static void main(String[] args) {
