a0ad168ef32e5778cb70c1db884e8962f5ab2fdb
elpharaexd <elpharaexd@outlook.com>
2021-09-27 15:48:01 +0200

    Hotovy program

--- src/pkg/Main.java
+++ src/pkg/Main.java
@@ -1,25 +1,59 @@
 package pkg;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private Scanner sc;
+
     /*
-    //TODO 1. úkol
+    1. úkol
     Převod galonů na litry -> výsledek vypsat do konzole.
     Proměnná, kterou si určíte (vstupní hodnota) bude počet galonů
     1 galon = 3.7854 litrů
+    */
 
-    //TODO 2. úkol
+    private float galToLi(float gal) {
+        return gal / 3.7854f;
+    }
+
+    /*
+    2. úkol
     Program pro výpočet vaší váhy na měsíci -> výsledek vypsat do konzole.
     Vstupní hodnota bude vaše váha na zemi
     Gravitace na měsíci odpovídá 17% pozemské
+    */
 
-    //TODO 3. úkol
+    private float moonWeight(float weight) {
+        return weight * .17f;
+    }
+
+    /*
+    3. úkol
     Napište program pro vypočet obsahu a obvodu kruhu -> výsledky vypsat do konzole.
     Vstupní hodnota bude poloměr kruhu
-     */
+    */
+
+    private double[] perimAndArea(double radius) {
+        return new double[]{2 * Math.PI * radius, Math.PI * Math.pow(radius, 2)};
+    }
+
+    private Main() {
+        this.sc = new Scanner(System.in);
+
+        System.out.print("Zadej galony: ");
+        System.out.println(galToLi(sc.nextFloat()));
+
+        System.out.print("Zadej svou váhu na zemi: ");
+        System.out.println(moonWeight(sc.nextFloat()));
+
+        System.out.print("Zadej poloměr kruhu: ");
+        double[] result = this.perimAndArea(sc.nextDouble());
+        System.out.println("Obvod: " + result[0] + ", Obsah: " + result[1]);
+    }
 
     public static void main(String[] args) {
-	// write your code here
+	    new Main();
     }
-    
+
 }
