bfb0b893c7eac6e3f05b245f81c9ea1aa314549c
elpharaexd <elpharaexd@outlook.com>
2021-09-27 15:24:37 +0200

    Initial commit

---
+++ src/pkg/Main.java
@@ -0,0 +1,25 @@
+package pkg;
+
+public class Main {
+
+    /*
+    //TODO 1. úkol
+    Převod galonů na litry -> výsledek vypsat do konzole.
+    Proměnná, kterou si určíte (vstupní hodnota) bude počet galonů
+    1 galon = 3.7854 litrů
+
+    //TODO 2. úkol
+    Program pro výpočet vaší váhy na měsíci -> výsledek vypsat do konzole.
+    Vstupní hodnota bude vaše váha na zemi
+    Gravitace na měsíci odpovídá 17% pozemské
+
+    //TODO 3. úkol
+    Napište program pro vypočet obsahu a obvodu kruhu -> výsledky vypsat do konzole.
+    Vstupní hodnota bude poloměr kruhu
+     */
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    
+}
